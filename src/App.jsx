6f78d1946de79a890f3c16f2ff76820f746a4cdb
andr92h1 <andr92h1@edu.easj.dk>
2021-09-13 15:05:46 +0200

    Checkboxes for TODO app

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+
+
+
+function App() {
+
+    return (
+
+        <div>
+<input type ="checkbox"/><p>Code</p>
+
+<input type ="checkbox" /><p>Work Out</p>
+
+<input type ="checkbox" /><p>Work</p>
+
+<input type ="checkbox" /><p>Improve as tattoo artist</p>
+
+        </div>
+    )
+}
+
+export default App;
